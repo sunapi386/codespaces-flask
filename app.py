97a7aa000587ecbe7a10e5a204aa1f1749836915
Jason Sun <1029022+sunapi386@users.noreply.github.com>
2023-03-23 15:42:17 +0000

    Add about page

--- app.py
+++ app.py
@@ -5,3 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
